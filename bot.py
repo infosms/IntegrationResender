2350e629c0123c77b3fb3d5ee71e2c5fb8e48dba
Alikhan <khan.dosaly@gmail.com>
2022-12-30 00:52:59 +0600

    added utemisov url path

--- bot.py
+++ bot.py
@@ -214,7 +214,8 @@ async def resend(message: types.Message):
         'kaznmu': 'https://salemoffice.kaznmu.edu.kz/jenkins/buildByToken/build?job=salemoffice-kaznmu&token=BUILD',
         'qazexpo': 'https://doc.qazexpocongress.kz/jenkins/buildByToken/build?job=salemoffice-qec&token=BUILD',
         'kaznu': 'https://odo.kaznu.kz/jenkins/buildByToken/build?job=KazNU&token=BUILD',
-        'zkatu': 'https://salemoffice.wkau.kz/jenkins/buildByToken/build?job=zkatu&token=BUILD'
+        'zkatu': 'https://salemoffice.wkau.kz/jenkins/buildByToken/build?job=zkatu&token=BUILD',
+        'utemisov': 'https://salemoffice.wku.edu.kz/jenkins/buildByToken/build?job=utemisov&token=BUILD'
     }
 
     servers = list(set([x for x in message.get_args().split(' ') if x in domains.keys()]))
